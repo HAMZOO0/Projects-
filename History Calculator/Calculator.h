4e098b918554001860ff3b369ed9fe0b5c1f86de
Hamza Khan <98114762+HAMZOO0@users.noreply.github.com>
2022-09-13 23:38:14 -0700

    Add files via upload

---
+++ History Calculator/Calculator.h
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<conio.h>
+#include<cmath>
+#include<fstream>
+using std::endl;
+using std::cin;
+using std::cout;
+// using std::;
+void CUI() // inter face for calculator 
+{
+    cout<<".....................................................................\n";
+    cout<<"\t\t+ - MIRO Calculator * /\n";
+    cout<<".....................................................................\n\n";
+    cout<<"\t 1. Addition "<<endl;
+    cout<<"\t 2. Subtraction "<<endl;
+    cout<<"\t 3. Multiplication "<<endl;
+    cout<<"\t 4. Division "<<endl;
+    cout<<"\t 5. Modulus  "<<endl;
+    cout<<"\t 6. Sin "<<endl;
+    cout<<"\t 7. Cos "<<endl;
+    cout<<"\t 8. Tan "<<endl;
+    cout<<"\t 9. Sin^-1 "<<endl;
+    cout<<"\t 10. Cos^-1 "<<endl;
+    cout<<"\t 11. Log  "<<endl;
+    cout<<"\t 12. Square Root  "<<endl;
+    cout<<"\t 13. Power   "<<endl;
+    
+}
